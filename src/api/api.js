076a932770b987474a811b24cdf84f6f73e0e529
mauricio-alves <mauricioalvesnutri@gmail.com>
2024-02-25 13:18:47 -0300

    feature: added netlify url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:3001",
-  production: "",
+  production: "http://teste-carvalho-aleixo-inc.netlify.app",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
